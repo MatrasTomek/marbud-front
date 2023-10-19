68dbe18600f99a63394f439dfdeeecaa5d2ceea5
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-10-19 21:32:34 +0200

    Add Prime ng lib and ad menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeIcons, MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'marbud-front-main';
+
+  items!: MenuItem[];
+  activeItem!: MenuItem;
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: PrimeIcons.HOME },
+      { label: 'Calendar', icon: 'pi pi-fw pi-calendar' },
+      { label: 'Edit', icon: 'pi pi-fw pi-pencil' },
+      { label: 'Documentation', icon: 'pi pi-fw pi-file' },
+      { label: 'Settings', icon: 'pi pi-fw pi-cog' },
+    ];
+    this.activeItem = this.items[0];
+  }
 }
