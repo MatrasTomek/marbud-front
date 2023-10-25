2d4d9363475eef4a10d71a495f40468cfc25afe0
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-10-25 22:50:07 +0200

    Add basic start-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenubarModule } from 'primeng/menubar';
+import { TabViewModule } from 'primeng/tabview';
+import { AccordionModule } from 'primeng/accordion';
 import { StartComponent } from './pages/start/start.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   declarations: [AppComponent, StartComponent, AboutUsComponent],
-  imports: [BrowserModule, AppRoutingModule, MenubarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MenubarModule,
+    TabViewModule,
+    AccordionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/start/start.component.ts
+++ src/app/pages/start/start.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.scss']
+  styleUrls: ['./start.component.scss'],
 })
-export class StartComponent {
-
-}
+export class StartComponent {}
