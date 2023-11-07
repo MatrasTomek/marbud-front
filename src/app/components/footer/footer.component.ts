f5f4777955a3bcfdc1e3398b5ba339f78fde9422
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-11-07 22:42:45 +0100

    Change footer layout

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,25 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component } from '@angular/core';
+import { footerData } from '../../../assets/constansData/footerData';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-  items!: MenuItem[];
+export class FooterComponent {
+  footerData;
 
-  ngOnInit() {
-    this.items = [
-      {
-        icon: 'pi pi-fw pi-phone',
-        label: '555-555-555',
-      },
-      {
-        icon: 'pi pi-fw pi-user',
-        routerLink: 'about-us',
-      },
-      { icon: 'pi pi-fw pi-map', routerLink: '' },
-    ];
+  constructor() {
+    this.footerData = footerData;
   }
 }
---
+++ src/assets/constansData/footerData.ts
@@ -0,0 +1,13 @@
+export const footerData = [
+  {
+    icon: 'pi pi-fw pi-phone',
+    label: '555-555-555',
+    routerLink: '',
+  },
+  {
+    icon: 'pi pi-fw pi-user',
+    label: '',
+    routerLink: 'about-us',
+  },
+  { icon: 'pi pi-fw pi-map', label: '', routerLink: '' },
+];
